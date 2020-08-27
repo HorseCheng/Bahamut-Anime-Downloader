1daa9fa355440f55e1faa75ee0df9ab5e7984c25
HorseCheng <ihorsechang@yahoo.com>
2020-08-27 22:21:19 +0800

    Upload code & README.md

--- README.md
+++ README.md
@@ -1,1 +1,122 @@
-# Bahamut-Anime-Downloader
+## 使用程式語言
+- Python3
+
+### 套件需求
+- Crypto
+- requests
+- re
+- os
+- natsort
+- shutil
+- time
+
+## 額外事前準備
+### A. 下載ffmpeg軟體 並設為系統變數
+1. 將分段ts檔影片製作成一部完整影片需要 ffmpeg 軟體
+請至ffmpeg官網下載執行檔
+**ffmpeg官網**: [官網下載連結](https://ffmpeg.org/download.html#build-windows)
+2. 下載完後需要將ffmpeg執行檔所屬之資料夾設為 **系統變數** 
+在Windows搜尋介面中打上 **環境變數** 並打開 **編輯系統環境變數** 
+ ![教學](https://i.imgur.com/O6KZ9IX.png)
+3. 點擊右下角之**環境變數**
+ ![教學](https://i.imgur.com/ps8K521.png)
+4. 開啟在下方 **系統變數** 的 **Path**選項 (點擊兩次)
+![教學](https://i.imgur.com/Ap1018Y.png)
+5. 點擊**新增** 並將剛剛下載ffmpeg檔案的**bin**資料夾路徑填寫在下面後一直點擊**確定**
+![教學](https://i.imgur.com/fkJjVoY.png)
+6. 這麼一來當你在**命令提示字元/cmd** 中打上ffmpeg 接上指令，系統就會知道要去呼叫剛剛下載ffmpeg檔案的**bin**資料夾內的ffmpeg.exe並做你想要做的處理
+### B. 將 cookie 複製到 cookie.txt
+1. 下載檔案時巴哈動畫瘋網站會依照你的cookie，決定你的身分，並判斷觀看影片前是否需要觀看廣告、及是否可以觀看1080P動畫
+請開啟**Chrome瀏覽器**並按**F12**開啟**開發人員工具**，並點擊**Network**頁面
+2. 連線到**巴哈動畫瘋官網**首頁: [官網連結](https://ani.gamer.com.tw/) 
+並在Network頁面選擇**ani.agmer.com.tw**之欄位
+![教學](https://i.imgur.com/lm85wlR.png)
+3. 滑到底部，並複製cookie欄後的文字 (例如以下方圖為例: 複製 __cfduid ~ 933 區段) 
+(註: 若有正確登入，通常cookie資訊會非常長)
+![教學](https://i.imgur.com/6U6Me31.png)
+4. 打開本repo的資料夾，並貼上到**cookie.txt**檔案
+5. 註: cookie具有**時效性**，若過了幾小時後需要再次使用本程式，需要再重複做**上述B區塊**的動作
+
+## 輸入教學
+1. 一開始請輸入指令+想下載的動畫**sn編號**:
+**sn編號**在巴哈動畫瘋影片**網址**的**最後面**
+![教學](https://imgur.com/D8kAY8M.png)
+**指令** 列表如下(如果只想下載**單一影片**，則可以直接打上sn編號，**前面可以不用加上指令**)
+```no-highlight
+    -a 下載當季所有動畫(all)
+    -b 下載當季第一集至該集動畫(beginning)
+    -e 下載當季該集至最後一集動畫(end)
+    -o 只下載該集動畫(one)
+    -m 後面接續多個該季其他想下載的集數(後面請以空白做區隔)(multiple)
+兩個範例:
+    -b 16870
+    -m 16870 37 38 39
+```   
+2. 等待30秒的廣告時間後(如果無巴哈動畫瘋VIP會員)，需要輸入影片下載的解析度，可以只填寫提示框的部分內容即可。如果第一步驟是選擇下載多部影片，只有下載第一部的時候需要輸入下載解析度，後續會以相同解析度做下載
+範例: 
+```no-highlight
+640x360
+960x540
+1280x720
+請選擇下載解析度 (AXB可輸入A即可): 1280
+```   
+
+## 執行結果範例
+```no-highlight
+請輸入指令+想下載的動畫sn號碼:
+    -a 下載當季所有動畫(all)
+    -b 下載當季第一集至該集動畫(beginning)
+    -e 下載當季該集至最後一集動畫(end)
+    -o 只下載該集動畫(one)
+    -m 後面接續多個該季其他想下載的集數(後面請以空白做區隔)(multiple)
+    範例:
+    -b 16870
+    -m 16870 37 38 39
+-a 16444
+一共有 84 集需要下載
+下載清單(sn): ['16444', '16445', '16446', '16447', '16448', '16449', '16450', '16451', '16452', '16453', '16454', '16455', '16456', '16457', '16458', '16459', '16460', '16461', '16462', '16463', '16464', '16465', '16466', '16467', '16468', '16469', '16470', '16471', '16472', '16473', '16474', '16475', '16476', '16477', '16478', '16479', '16480', '16481', '16482', '16483', '16537', '16538', '16539', '16540', '16541', '16542', '16543', '16544', '16545', '16546', '16547', '16548', '16549', '16550', '16551', '16552', '16553', '16554', '16555', '16556', '16564', '16565', '16566', '16567', '16568', '16569', '16570', '16571', '16572', '16573', '16574', '16575', '16576', '16577', '16578', '16579', '16580', '16581', '16582', '16583', '16584', '16585', '16586', '16587']
+
+即將下載... 16444: '寶可夢超級願望 [1]'
+等待 30 秒 廣告時間中...
+640x360
+960x540
+1280x720
+請選擇下載解析度 (AXB可輸入A即可): 1280
+
+ffmpeg version git-2020-08-18-1c7e55d Copyright (c) 2000-2020 the FFmpeg developers
+  built with gcc 10.2.1 (GCC) 20200805
+  configuration: --enable-gpl --enable-version3 --enable-sdl2 --enable-fontconfig --enable-gnutls --enable-iconv --enable-libass --enable-libdav1d --enable-libbluray --enable-libfreetype --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libopenjpeg --enable-libopus --enable-libshine --enable-libsnappy --enable-libsoxr --enable-libsrt --enable-libtheora --enable-libtwolame --enable-libvpx --enable-libwavpack --enable-libwebp --enable-libx264 --enable-libx265 --enable-libxml2 --enable-libzimg --enable-lzma --enable-zlib --enable-gmp --enable-libvidstab --enable-libvmaf --enable-libvorbis --enable-libvo-amrwbenc --enable-libmysofa --enable-libspeex --enable-libxvid --enable-libaom --enable-libgsm --enable-librav1e --enable-libsvtav1 --disable-w32threads --enable-libmfx --enable-ffnvcodec --enable-cuda-llvm --enable-cuvid --enable-d3d11va --enable-nvenc --enable-nvdec --enable-dxva2 --enable-avisynth --enable-libopenmpt --enable-amf
+  libavutil      56. 58.100 / 56. 58.100
+  libavcodec     58.100.100 / 58.100.100
+  libavformat    58. 51.100 / 58. 51.100
+  libavdevice    58. 11.101 / 58. 11.101
+  libavfilter     7. 87.100 /  7. 87.100
+  libswscale      5.  8.100 /  5.  8.100
+  libswresample   3.  8.100 /  3.  8.100
+  libpostproc    55.  8.100 / 55.  8.100
+Input #0, concat, from 'dump/list.txt':
+  Duration: N/A, start: 0.000000, bitrate: 213 kb/s
+    Stream #0:0: Video: h264 (High), yuv420p(progressive), 1280x720 [SAR 1:1 DAR 16:9], 30 fps, 30 tbr, 90k tbn, 60 tbc
+    Stream #0:1: Audio: aac (LC), 48000 Hz, stereo, fltp, 213 kb/s
+Output #0, mp4, to 'output/寶可夢超級願望 [1].mp4':
+  Metadata:
+    encoder         : Lavf58.51.100
+    Stream #0:0: Video: h264 (High) (avc1 / 0x31637661), yuv420p(progressive), 1280x720 [SAR 1:1 DAR 16:9], q=2-31, 30 fps, 30 tbr, 90k tbn, 90k tbc
+    Stream #0:1: Audio: aac (LC) (mp4a / 0x6134706D), 48000 Hz, stereo, fltp, 213 kb/s
+Stream mapping:
+  Stream #0:0 -> #0:0 (copy)
+  Stream #0:1 -> #0:1 (copy)
+Press [q] to stop, [?] for help
+frame=44270 fps=3398 q=-1.0 Lsize=  217809kB time=00:24:35.66 bitrate=1209.1kbits/s speed= 113x
+video:181832kB audio:35199kB subtitle:0kB other streams:0kB global headers:0kB muxing overhead: 0.358080%
+
+即將下載... 16445: '寶可夢超級願望 [2]'
+等待 30 秒 廣告時間中...# Bahamut-Anime-Downloader
+
+...
+```
+
+---
+
+## 警語⚠️
+本程式僅供作為**Python程式語言應用範例**之教學用途，請勿使用於任何非法行為
---
+++ baha.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+
+from Crypto.Cipher import AES
+import requests
+import re
+import os
+import natsort
+import shutil
+import time
+
+cookie=""
+with open ("cookie.txt",'r') as f:
+    cookie=f.read().strip("\n")
+if cookie=="":
+    print("請記得將cookie填寫至cookie.txt\n") 
+    import sys
+    sys.exit()
+
+sninput=input("請輸入指令+想下載的動畫sn號碼: \n\
+    -a 下載當季所有動畫(all)\n\
+    -b 下載當季第一集至該集動畫(beginning)\n\
+    -e 下載當季該集至最後一集動畫(end)\n\
+    -o 只下載該集動畫(one)\n\
+    -m 後面接續多個該季其他想下載的集數(後面請以空白做區隔)(multiple)\n\
+    範例:\n\
+    -b 16870\n\
+    -m 16870 37 38 39\n")
+sn=re.findall("[0-9]+",sninput)[0]
+
+headers={"sec-fetch-dest":"empty","sec-fetch-mode":"cors","sec-fetch-site":"same-origin","dnt":"1","user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"}   
+headers["cookie"] = cookie 
+headers["origin"] ="https://ani.gamer.com.tw"
+headers["referer"] = f"https://ani.gamer.com.tw/animeVideo.php?sn={sn}"
+
+finder=requests.get(f"https://ani.gamer.com.tw/animeVideo.php?sn={sn}",headers=headers)
+downloadlist=re.findall('<a href="\?sn=(.*?)">.*?</a>', finder.text)
+namelist=re.findall('<a href="\?sn=.*?">(.*?)</a>', finder.text)
+index=downloadlist.index(sn)
+
+if ("-a" in sninput):
+    downloadlist = downloadlist
+elif ("-b" in sninput):
+    downloadlist = downloadlist[0:index+1]
+elif ("-e" in sninput):
+    downloadlist = downloadlist[index:]
+elif ("-m" in sninput):
+    temp=[sn]
+    inputs=re.findall("[0-9.]+",sninput)[1:]
+    for i in inputs:
+        temp.append(downloadlist[namelist.index(i)])
+    downloadlist = temp
+else:
+    downloadlist = [ downloadlist[index] ]
+
+downloadlist = list(sorted(set(downloadlist)))
+print(f"一共有 {len(downloadlist)} 集需要下載")
+print(f"下載清單(sn): {downloadlist}")
+
+deviceid= requests.get("https://ani.gamer.com.tw/ajax/getdeviceid.php",headers=headers).json()["deviceid"]
+
+times=0
+reschoice=-1 #Download highest resolution
+for times, i in enumerate(downloadlist):
+    sn=i
+    html=requests.get(f"https://ani.gamer.com.tw/animeVideo.php?sn={sn}",headers=headers)
+    filename=re.findall("<title>(.*) 線上看 - 巴哈姆特動畫瘋</title>" , html.text)[0]
+    print(f"\n即將下載... {sn}: '{filename}'")
+
+    vip=requests.get(f"https://ani.gamer.com.tw/ajax/token.php?adID=0&sn={sn}&device={deviceid}",headers=headers).json()["vip"]
+    
+    requests.get(f"https://ani.gamer.com.tw/ajax/unlock.php?sn={sn}&ttl=0",headers=headers) #unlock
+    requests.get(f"https://ani.gamer.com.tw/ajax/checklock.php?device={deviceid}&sn={sn}",headers=headers) #checklock
+
+    if(not vip ):
+        print("等待 30 秒 廣告時間中...")
+        requests.get(f"https://ani.gamer.com.tw/ajax/unlock.php?sn={sn}&ttl=0",headers=headers) #unlock
+        requests.get(f"https://ani.gamer.com.tw/ajax/unlock.php?sn={sn}&ttl=0",headers=headers) #unlock
+        requests.get(f"https://ani.gamer.com.tw/ajax/videoCastcishu.php?sn={sn}&s=194699",headers=headers)
+        time.sleep(10)
+        requests.get(f"https://ani.gamer.com.tw/ajax/unlock.php?sn={sn}&ttl=0",headers=headers) #unlock
+        requests.get(f"https://ani.gamer.com.tw/ajax/unlock.php?sn={sn}&ttl=0",headers=headers) #unlock
+        time.sleep(20)  
+        requests.get(f"https://ani.gamer.com.tw/ajax/videoCastcishu.php?sn={sn}&s=194699&ad=end",headers=headers)
+    time.sleep(1)  
+    requests.get(f"https://ani.gamer.com.tw/ajax/videoStart.php?sn={sn}",headers=headers)
+    try:
+        resolutionurl=requests.get(f"https://ani.gamer.com.tw/ajax/m3u8.php?sn={sn}&device={deviceid}",headers=headers).json()["src"]
+    except:
+        try:
+            requests.get(f"https://ani.gamer.com.tw/ajax/videoCastcishu.php?sn={sn}&s=194699&ad=end",headers=headers)
+            requests.get(f"https://ani.gamer.com.tw/ajax/videoStart.php?sn={sn}",headers=headers)
+            resolutionurl=requests.get(f"https://ani.gamer.com.tw/ajax/m3u8.php?sn={sn}&device={deviceid}",headers=headers).json()["src"]
+        except:
+            resolutionurl=requests.get(f"https://ani.gamer.com.tw/ajax/m3u8.php?sn={sn}&device={deviceid}",headers=headers)
+            print(resolutionurl.json()["error"]["message"])
+            print("可嘗試更新cookie資訊")
+            import sys
+            sys.exit(0)
+        
+    #resolutionurl="https:\/\/gamer-cds.cdn.hinet.net\/vod_gamer\/_definst_\/smil:gamer2\/1105960c61553a4166f4b71bfb2e8fb9cb2710df\/hls-s-ae-2s.smil\/playlist.m3u8?token=s8ub07nihuER0aQDTkPPzA&expires=1598302260&bahaData=HorseCheng%3A16870%3A0%3APC%3A25761"
+    resolutionm3u8=requests.get(resolutionurl.replace("\\",""), headers=headers).content
+    with open("resolution.m3u8","wb") as f:
+        f.write(resolutionm3u8)
+        
+    resolution=""
+    with open("resolution.m3u8","r") as f:
+        resolution=f.read()
+    
+    innlist = re.findall("(chunklist_.*)\n", resolution)
+    reslist = re.findall("RESOLUTION=(.*)", resolution)
+    if (times==0):
+        for res in reslist:
+           print(f"{res}")
+        reschoice=input("請選擇下載解析度 (AXB可輸入A即可): ")  
+    try:
+        matching=[i for i, e in enumerate(reslist) if (reschoice in e) ] [0]
+        inn=resolutionurl.replace( "playlist", re.findall("chunklist_b[0-9]+", innlist[matching])[0] )
+    except :
+        if(times==0):print("輸入與選項不符 自動選擇下載最高解析度")
+        inn=resolutionurl.replace( "playlist", re.findall("chunklist_b[0-9]+", innlist[-1])[0] )
+    
+    print()
+    try: os.makedirs("temp")
+    except:pass
+    try: os.makedirs("dump")
+    except:pass
+    try: os.makedirs("output")
+    except:pass
+    
+    title = re.findall(".*smil/*",inn)[0]
+    m3u8url = re.findall("chunklist.*",inn)[0]
+    
+    m3u8html=requests.get(title+m3u8url,headers=headers)
+    
+    num=0
+    if(m3u8html.status_code == 200):
+        with open("a.m3u8",'wb') as f:
+          f.write(m3u8html.content)
+    else: print("error!")
+    
+    with open ("a.m3u8","r") as f:
+        m3u8files=f.readlines()
+    
+    keyurl=re.findall('URI="(.*)"',m3u8files[4])[0]
+    key=requests.get(keyurl,headers=headers)
+    with open("a.key" ,"wb" ) as f:
+        f.write(key.content)
+    
+    with open("a.key","rb") as f:
+        key=f.read()
+        
+    length=int ( ((len(m3u8files)-7)+0.5) //2 )
+    cnt=1
+    for i in range(6,len(m3u8files),2):
+        target=m3u8files[i]
+        print(f"downloading {cnt}/{length}", end='\r', flush=True)
+        content=requests.get(title+target,headers=headers, stream=True)    
+        num=0
+        if(content.status_code == 200):
+            with open(f"temp/a_{cnt}.ts",'wb') as f:
+             	f.write(content.content)
+            cnt+=1
+        else: print("error!")
+        
+    filelist=""
+    for i in natsort.natsorted(os.listdir('temp')):  
+        video=None
+        with open(f"temp/{i}","rb") as f:
+            video=f.read()
+        cryptor = AES.new(key, AES.MODE_CBC,video[0:16])
+        with open (f"dump/{i}", "wb") as f:
+            f.write(cryptor.decrypt(video))
+        print(f"{i} is decrypted!", end='\r', flush=True)
+        filelist+=f"file {i}\n"
+        
+    with open("dump/list.txt","w") as f:
+        f.write(filelist)
+    
+    os.system(f'ffmpeg -y -f concat -i "dump/list.txt" -c copy "output/{filename}.mp4"') 
+    
+    try:shutil.rmtree("dump")
+    except:pass
+    try: shutil.rmtree("temp")
+    except:pass
